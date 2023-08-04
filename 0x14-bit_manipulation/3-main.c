7a0a2a2f80f70b7aea021e68cb0b66c9afe97922
888Greys <toxicgreys001@gmail.com>
2023-08-04 06:12:54 -0700

    Bit 3

--- 0x14-bit_manipulation/3-main.c
+++ 0x14-bit_manipulation/3-main.c
@@ -1,33 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
-/**
- * set_bit - Sets a bit at a given index to 1.
- * @n: Pointer to the unsigned long integer.
- * @index: The index of the bit to set (0-indexed, from right to left).
- */
-void set_bit(unsigned long int *n, unsigned int index);
-#endif
-#include "main.h"
-/**
- * set_bit - Sets a bit at a given index to 1.
- * @n: Pointer to the unsigned long integer.
- * @index: The index of the bit to set (0-indexed, from right to left).
- */
-void set_bit(unsigned long int *n, unsigned int index)
-{
-unsigned long int mask = 1UL << index;
-*n |= mask;
-}
 #include <stdio.h>
 #include "main.h"
+
 /**
- * main - Entry point of the program.
+ * main - check the algorithm
  *
  * Return: Always 0.
  */
 int main(void)
 {
 unsigned long int n;
+
 n = 1024;
 set_bit(&n, 5);
 printf("%lu\n", n);
@@ -39,4 +21,3 @@ set_bit(&n, 0);
 printf("%lu\n", n);
 return (0);
 }
-
