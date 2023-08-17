f4b509980ca5e161604a13732633a7514237e837
888Greys <toxicgreys001@gmail.com>
2023-08-17 15:46:46 -0700

    Deleting an index'

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,44 +1,34 @@
 #include "lists.h"
+
 /**
- * delete_dnodeint_at_index - deletes the node at index of a in the linked list
- * dlistint_t linked list
- *
- * @head: header  of the list
- * @index: index of the new node in the linked list
- * Return: 1 if it succeeded, -1 if it failed
+ * delete_dnodeint_at_index - deletes node at specific index
+ * @head: header of linked list
+ * @index: index of the node to delete at the linked list
+ * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-dlistint_t *hh1;
-dlistint_t *hh2;
-unsigned int i;
-hhh1 = *head;
-if (hh1 != NULL)
-while (hh1->prev != NULL)
-hh1 = hh1->prev;
-i = 0;
-while (hh1 != NULL)
+dlistint_t *current;
+if (head == NULL)
+return (-1);
+current = *head;
+if (current == NULL)
+return (-1);
+while (index != 0)
 {
-if (i == index)
-{
-if (i == 0)
-{
-*head = hh1->next;
-if (*head != NULL)
-(*head)->prev = NULL;
+current = current->next;
+index--;
+if (current == NULL)
+return (-1);
 }
+if (current->prev != NULL)
+current->prev->next = current->next;
 else
 {
-hh2->next = hh1->next;
-if (hh1->next != NULL)
-hh1->next->prev = hh2;
+*head = current->next;
+if (current->next != NULL)
+current->next->prev = NULL;
+free(current);
 }
-free(hh1);
 return (1);
-}
-hh2 = hh1;
-hh1 = hh1->next;
-i++;
-}
-return (-1);
 }
