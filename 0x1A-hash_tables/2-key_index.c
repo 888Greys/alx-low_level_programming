1b6e7cd10c58b29534621d66a0356b30b99c1243
888Greys <toxicgreys001@gmail.com>
2023-09-13 08:07:14 -0700

    it gives the index of a key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,12 +9,12 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    /*
-     * This function calculates the index in a hash table's array for a given
-     * key using the djb2 hash algorithm. It takes the key and the size of the
-     * array as inputs, computes the hash value using hash_djb2(key), and then
-     * calculates the index by taking the modulus of the hash value with the
-     * size of the array. The resulting index is returned.
-     */
-    return (hash_djb2(key) % size);
+/*
+* This function calculates the index in a hash table's array for a given
+* key using the djb2 hash algorithm. It takes the key and the size of the
+* array as inputs, computes the hash value using hash_djb2(key), and then
+* calculates the index by taking the modulus of the hash value with the
+* size of the array. The resulting index is returned.
+*/
+return (hash_djb2(key) % size);
 }
